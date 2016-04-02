ae2d54674cba3ecbf2e8460182e8edcc674b1685
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-04-02 16:48:36 +0200

    [ACC-416] add sdsl

--- ds/dsl/src/main/scala/net/echinopsii/ariane/community/core/mapping/ds/sdsl/internal/Expression.scala
+++ ds/dsl/src/main/scala/net/echinopsii/ariane/community/core/mapping/ds/sdsl/internal/Expression.scala
@@ -1,8 +1,31 @@
+/**
+ * Mapping Selector
+ * Mapping Selector
+ * Copyright (C) 01/04/16 echinopsii
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.echinopsii.ariane.community.core.mapping.ds.sdsl.internal
 
-/**
- * Created by mffrench on 4/2/16.
- */
-class Expression {
+abstract class Expression() {
+  var eType: String
 
+  def calcType : String
+}
+
+case class StringExp(value: String) extends Expression {
+  override var eType: String = "String"
+
+  override def calcType: String = eType
 }
